c2a3275ed92717334caa05fd54849e2065e609cb
Sane Aalam <saneraza78692@gmail.com>
2025-04-16 23:12:06 +0530

    install: tailwind-css setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
+simport "./App.css";
 
 function App() {
-  return <div className="App">Hello world!</div>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
